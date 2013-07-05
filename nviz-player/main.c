19c51157ee7cfd7c81997cc8831e3f696cee20c1
nikola <nikola.whallon@gmail.com>
2013-07-05 19:25:25 +0900

    major update to nviz-player; nviz file format change; other programs must be updated

--- nviz-player/main.c
+++ nviz-player/main.c
@@ -3,63 +3,63 @@
 
 #include <stdio.h>
 #include <stdint.h>
-#include <string.h>
 #include <ncurses.h>
 #include <semaphore.h>
 #include <pthread.h>
 #include <signal.h>
 
-#define MAX_FPS 96
-#define MAX_COL 200
-#define MAX_ROW 100
+#define NVIZ_HD 5
 #define CH_BYTS	2
+#define MAX_COL 250
+#define MAX_ROW 75
+#define MAX_FPS 96
 
 typedef struct {
-	int running;				// bool
-	int32_t read_frame_index;
-	char * frame_pool;
-	char * nviz_file_path;
-	int32_t nviz_col;
-	int32_t nviz_row;
-	int32_t nviz_fps;
-	int32_t nviz_fno;
-	sem_t * switch_sem;
-	sem_t * read_sem;
+	int t_running;
+	char * t_frame_pool;
+	char * t_nviz_file_path;
+	int t_nviz_col;
+	int t_nviz_row;
+	int t_nviz_fps;
+	int t_nviz_sec;
+	sem_t * t_switch_sem;
+	sem_t * t_read_sem;
+	int32_t t_read_frame_index;
 } thread_info;
 
 //----------------------------------------------------				// GLOBAL VARIABLES
 
 // control
 int g_ch;
-int g_running = 1;				// bool
+int g_running = 1;
 
 // nviz control
-int g_paused;					// bool
-int g_looping;					// bool
-int g_just_reset;				// bool
+int g_paused;
+int g_looping;
+int g_just_reset;
 int32_t g_render_frame_index;
 int32_t g_rewind_fast_forward_rate;
 
 // ncurses
 int g_col, g_row;
-int g_color_mode;				// bool
+int g_color_mode;
 
 // frame pool
-char g_frame_pool_0[CH_BYTS * (MAX_FPS * MAX_COL * MAX_ROW)];
-char g_frame_pool_1[CH_BYTS * (MAX_FPS * MAX_COL * MAX_ROW)];
-int g_frame_pool_rendering;			// bool
-int g_frame_pool_reading;			// bool
+char g_frame_pool_0[CH_BYTS * (MAX_COL * MAX_ROW) * MAX_FPS];
+char g_frame_pool_1[CH_BYTS * (MAX_COL * MAX_ROW) * MAX_FPS];
+int g_frame_pool_rendering;
+int g_frame_pool_reading;
 
 // nviz
 char g_nviz_file_path[256];
-int32_t g_nviz_col;
-int32_t g_nviz_row;
-int32_t g_nviz_fps;
-int32_t g_nviz_fno;
+int g_nviz_col;
+int g_nviz_row;
+int g_nviz_fps;
+int g_nviz_sec;
 
 // panels
-int g_hide_panel = 0;				// bool
-int g_info_control_panel = 0;			// bool 0 = info, 1 = control
+int g_hide_panel = 0;
+int g_info_control_panel = 0;
 
 // semaphore
 sem_t g_switch_sem;
@@ -117,11 +117,15 @@ void init_ncurses()
 	}
 
 	getmaxyx(stdscr, g_row, g_col);			// get the col x row of the terminal
+
+	clear();
 }
 
 // deinitialize ncurses
 void deinit_ncurses()
 {
+	clear();
+
 	endwin();
 }
 
@@ -151,14 +155,14 @@ void switch_frame_pools(int32_t _read_frame_index)
 
 	if (g_frame_pool_reading == 0)
 	{
-		g_thread_info.frame_pool = g_frame_pool_0;
+		g_thread_info.t_frame_pool = g_frame_pool_0;
 	}
 	else
 	{
-		g_thread_info.frame_pool = g_frame_pool_1;
+		g_thread_info.t_frame_pool = g_frame_pool_1;
 	}
 
-	g_thread_info.read_frame_index = _read_frame_index;
+	g_thread_info.t_read_frame_index = _read_frame_index;
 
 	sem_post(&g_read_sem);
 }
@@ -168,18 +172,18 @@ static void * read_frames(void * param)
 {
 	thread_info * ti = (thread_info *) param;
 
-	while (ti->running)
+	while (ti->t_running)
 	{
-		sem_wait(ti->read_sem);
+		sem_wait(ti->t_read_sem);
 
-		FILE * nviz_file = fopen(ti->nviz_file_path, "rb");
+		FILE * nviz_file = fopen(ti->t_nviz_file_path, "rb");
 
-		fseek(nviz_file, 4 * 4 + CH_BYTS * (ti->read_frame_index * ti->nviz_col * ti->nviz_row), SEEK_SET);
-		fread(ti->frame_pool, 1, CH_BYTS * (ti->nviz_col * ti->nviz_row * ti->nviz_fps), nviz_file);
+		fseek(nviz_file, NVIZ_HD + CH_BYTS * (ti->t_nviz_col * ti->t_nviz_row) * ti->t_read_frame_index, SEEK_SET);
+		fread(ti->t_frame_pool, 1, CH_BYTS * (ti->t_nviz_col * ti->t_nviz_row) * ti->t_nviz_fps, nviz_file);
 
 		fclose(nviz_file);
 
-		sem_post(ti->switch_sem);
+		sem_post(ti->t_switch_sem);
 	}
 }
 
@@ -188,7 +192,7 @@ void reset_to_render_frame_index()
 {
 	switch_frame_pools(g_nviz_fps * (g_render_frame_index / g_nviz_fps));
 
-	if (g_nviz_fps * (g_render_frame_index / g_nviz_fps) + g_nviz_fps < g_nviz_fno)
+	if (g_nviz_fps * (g_render_frame_index / g_nviz_fps) + g_nviz_fps < g_nviz_fps * g_nviz_sec)
 	{
 		switch_frame_pools(g_nviz_fps * (g_render_frame_index / g_nviz_fps) + g_nviz_fps);
 	}
@@ -204,8 +208,6 @@ void reset_to_render_frame_index()
 int init_nviz()
 {
 	// frame pool
-	memset(g_frame_pool_0, 48, CH_BYTS * (MAX_FPS * MAX_COL * MAX_ROW));
-	memset(g_frame_pool_1, 48, CH_BYTS * (MAX_FPS * MAX_COL * MAX_ROW));
 	g_frame_pool_rendering = 0;
 	g_frame_pool_reading = 1;
 
@@ -215,7 +217,7 @@ int init_nviz()
 	g_render_frame_index = 0;
 	g_rewind_fast_forward_rate = 1;
 
-	// declare and open the video file
+	// declare and open the nviz file
 	FILE * nviz_file = fopen(g_nviz_file_path, "rb");
 
 	// check that the file could be opened
@@ -229,27 +231,27 @@ int init_nviz()
 	int32_t file_size = ftell(nviz_file);
 	fseek(nviz_file, 0, SEEK_SET);
 
-	// check that the file contains the video info
-	if (file_size < 4 * 4)
+	// check that the file contains the nviz info
+	if (file_size < NVIZ_HD)
 	{
 		fclose(nviz_file);
 		return 1;
 	}
 
-	// input the video info
-	fread(&g_nviz_col, 4, 1, nviz_file);
-	fread(&g_nviz_row, 4, 1, nviz_file);
-	fread(&g_nviz_fps, 4, 1, nviz_file);
-	fread(&g_nviz_fno, 4, 1, nviz_file);
+	// input the nviz info
+	fread(&g_nviz_col, 1, 1, nviz_file);
+	fread(&g_nviz_row, 1, 1, nviz_file);
+	fread(&g_nviz_fps, 1, 1, nviz_file);
+	fread(&g_nviz_sec, 2, 1, nviz_file);
 
-	// check that the file contains the video data
-	if (file_size < 4 * 4 + CH_BYTS * (g_nviz_col * g_nviz_row) * g_nviz_fno)
+	// check that the file contains the nviz data
+	if (file_size < NVIZ_HD + CH_BYTS * (g_nviz_col * g_nviz_row) * g_nviz_fps * g_nviz_sec)
 	{
 		fclose(nviz_file);
 		return 1;
 	}
 
-	// close the video file
+	// close the nviz file
 	fclose(nviz_file);
 
 	// semaphore
@@ -257,16 +259,16 @@ int init_nviz()
 	sem_init(&g_read_sem, 0, 0);
 
 	// thread
-	g_thread_info.running = 1;
-	g_thread_info.read_frame_index = 0;
-	g_thread_info.frame_pool = g_frame_pool_1;
-	g_thread_info.nviz_file_path = g_nviz_file_path;
-	g_thread_info.nviz_col = g_nviz_col;
-	g_thread_info.nviz_row = g_nviz_row;
-	g_thread_info.nviz_fps = g_nviz_fps;
-	g_thread_info.nviz_fno = g_nviz_fno;
-	g_thread_info.switch_sem = &g_switch_sem;
-	g_thread_info.read_sem = &g_read_sem;
+	g_thread_info.t_running = 1;
+	g_thread_info.t_frame_pool = g_frame_pool_1;
+	g_thread_info.t_nviz_file_path = g_nviz_file_path;
+	g_thread_info.t_nviz_col = g_nviz_col;
+	g_thread_info.t_nviz_row = g_nviz_row;
+	g_thread_info.t_nviz_fps = g_nviz_fps;
+	g_thread_info.t_nviz_sec = g_nviz_sec;
+	g_thread_info.t_switch_sem = &g_switch_sem;
+	g_thread_info.t_read_sem = &g_read_sem;
+	g_thread_info.t_read_frame_index = 0;
 
 	pthread_attr_init(&g_read_thread_attr);
 	pthread_attr_setstacksize(&g_read_thread_attr, 0x10000000);
@@ -281,7 +283,7 @@ int init_nviz()
 // deinitialize
 void deinit_nviz()
 {
-	g_thread_info.running = 0;
+	g_thread_info.t_running = 0;
 	sem_post(&g_read_sem);
 	pthread_join(g_read_thread_id, NULL);
 }
@@ -307,7 +309,7 @@ void up_rewind_fast_forward_rate()
 	{
 		g_rewind_fast_forward_rate = g_nviz_fps;
 	}
-	else if (g_rewind_fast_forward_rate != g_nviz_fno - g_nviz_fps)
+	else if (g_rewind_fast_forward_rate != g_nviz_fps * g_nviz_sec - g_nviz_fps)
 	{
 		g_rewind_fast_forward_rate += g_nviz_fps;
 	}
@@ -329,7 +331,7 @@ void down_rewind_fast_forward_rate()
 // rewind
 void rewind_nviz()
 {
-	uint32_t previous_frame_index = g_render_frame_index;
+	int32_t previous_frame_index = g_render_frame_index;
 
 	if (g_render_frame_index - g_rewind_fast_forward_rate >= 0)
 	{
@@ -349,15 +351,15 @@ void rewind_nviz()
 // fast forward
 void fast_forward_nviz()
 {
-	uint32_t previous_frame_index = g_render_frame_index;
+	int32_t previous_frame_index = g_render_frame_index;
 
-	if (g_render_frame_index + g_rewind_fast_forward_rate < g_nviz_fno)
+	if (g_render_frame_index + g_rewind_fast_forward_rate < g_nviz_fps * g_nviz_sec)
 	{
 		g_render_frame_index += g_rewind_fast_forward_rate;
 	}
 	else
 	{
-		g_render_frame_index = g_nviz_fno - 1;
+		g_render_frame_index = g_nviz_fps * g_nviz_sec - 1;
 	}
 
 	if (g_nviz_fps * (g_render_frame_index / g_nviz_fps) != g_nviz_fps * (previous_frame_index / g_nviz_fps))
@@ -369,14 +371,16 @@ void fast_forward_nviz()
 // render video
 void render()
 {
-	for (int r = 0; r < g_nviz_row; r++)
+	int c;
+	int r;
+	for (r = 0; r < g_nviz_row; r++)
 	{
-		for (int c = 0; c < g_nviz_col; c++)
+		for (c = 0; c < g_nviz_col; c++)
 		{
 			char chr;
 			char clr;
 
-			int32_t index = CH_BYTS * ((g_render_frame_index % g_nviz_fps) * g_nviz_row * g_nviz_col + r * g_nviz_col + c);
+			int32_t index = CH_BYTS * ((g_nviz_col * g_nviz_row) * (g_render_frame_index % g_nviz_fps) + g_nviz_col * r + c);
 
 			if (g_frame_pool_rendering == 1)
 			{
@@ -405,7 +409,8 @@ void render()
 
 	if (!g_hide_panel)
 	{
-		for (int i = 0; i < g_col; i++)
+		int i;
+		for (i = 0; i < g_col; i++)
 		{
 			mvaddch(g_row - 7, i, '-');
 		}
@@ -415,8 +420,8 @@ void render()
 		{
 			mvprintw(g_row - 6, 0, "col x row = %d x %d", g_nviz_col, g_nviz_row);
 			mvprintw(g_row - 5, 0, "fps = %d", g_nviz_fps);
-			mvprintw(g_row - 4, 0, "seconds = %d / %d\t", g_render_frame_index / g_nviz_fps, g_nviz_fno / g_nviz_fps);
-			mvprintw(g_row - 3, 0, "frames = %d / %d\t", g_render_frame_index, g_nviz_fno - 1);
+			mvprintw(g_row - 4, 0, "seconds = %d / %d\t", g_render_frame_index / g_nviz_fps, g_nviz_sec);
+			mvprintw(g_row - 3, 0, "frames = %d / %d\t", g_render_frame_index, g_nviz_fps * g_nviz_sec - 1);
 			mvprintw(g_row - 1, 0, "file = %s", g_nviz_file_path);
 		}
 
@@ -449,7 +454,7 @@ int main (int argc, char * argv[])
 	// command line input
 	if (argc != 2)
 	{
-		fprintf(stderr, "wrong number of arguments\n");
+		fprintf(stderr, "ERROR - wrong number of arguments\n");
 		fprintf(stderr, "usage: %s filename\n", argv[0]);
 		return 1;
 	}
@@ -460,14 +465,10 @@ int main (int argc, char * argv[])
 	init_ncurses();
 
 	// initialize nviz
-	clear();
 	if (init_nviz())
 	{
-		clear();
 		deinit_ncurses();
-
-		fprintf(stderr, "could not open %s\n", g_nviz_file_path);
-
+		fprintf(stderr, "ERROR - could not open %s\n", g_nviz_file_path);
 		return 1;
 	}
 
@@ -515,14 +516,14 @@ int main (int argc, char * argv[])
 		{
 			if (!g_just_reset)
 			{
-				if (g_render_frame_index + 1 > g_nviz_fno)
+				if (g_render_frame_index + 1 > g_nviz_fps * g_nviz_sec)
 				{
 					g_render_frame_index = 0;
 				}
 
 				if (g_render_frame_index % g_nviz_fps == 0)
 				{
-					if (g_render_frame_index + g_nviz_fps == g_nviz_fno)
+					if (g_render_frame_index + g_nviz_fps == g_nviz_fps * g_nviz_sec)
 					{
 						switch_frame_pools(0);
 					}
@@ -537,7 +538,7 @@ int main (int argc, char * argv[])
 				g_just_reset = 0;
 			}
 
-			if (!g_looping && g_render_frame_index == g_nviz_fno - 1)
+			if (!g_looping && g_render_frame_index == g_nviz_fps * g_nviz_sec - 1)
 			{
 				g_paused = 1;
 			}
@@ -557,7 +558,6 @@ int main (int argc, char * argv[])
 
 	// deinitialize nviz
 	deinit_nviz();
-	clear();
 
 	// deinitialize ncurses
 	deinit_ncurses();
